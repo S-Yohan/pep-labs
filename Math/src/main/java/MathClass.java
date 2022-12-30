548c18a569685f81bd1248dc9ffce0b8de7e1155
S-Yohan <forresters@merrimack.edu>
2022-12-30 00:24:14 +0000

    Saved progress on Math 2022-12-30 00:24:14 +0000

--- Math/src/main/java/MathClass.java
+++ Math/src/main/java/MathClass.java
@@ -1,4 +1,6 @@
 
+import java.lang.Math;
+
 public class MathClass {
     /**
      * The Math class contains utility methods that can be used for calculations like exponents, rounding, random
@@ -20,6 +22,6 @@ public class MathClass {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return 0;
+        return Math.pow (a,b);
     }
 }
