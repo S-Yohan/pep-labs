da89a4136728cc0789516c312078f707d5b50cc0
S-Yohan <forresters@merrimack.edu>
2022-12-31 03:01:20 +0000

    Saved progress on GuessingGame 2022-12-31 03:01:20 +0000

--- GuessingGame/test-output.sh
+++ GuessingGame/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running GameServiceTest
-Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.052 sec
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.136 sec
 
 Results :
 
