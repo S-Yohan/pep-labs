267d7b8ff2e6aa95ff2b53a8c0a0f7f09c509fb9
S-Yohan <forresters@merrimack.edu>
2023-01-01 17:55:45 +0000

    Saved progress on Lists 2023-01-01 17:55:45 +0000

--- Lists/src/main/java/ListExercise.java
+++ Lists/src/main/java/ListExercise.java
@@ -25,12 +25,14 @@ public class ListExercise {
      * not int? Generics can only contain Objects, and not primitives, so we Integer because it is a Wrapper class,
      * the Object form of a primitive.
      *
-     * A popular implementation of List is ArrayList. Look up how to instantiate one.
+     * A popular implementation of List is ArrayList. Look up how to instantiate one.(An interface CANNOT be instantiated so this is confusing terminology!!)
      *
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+
+        List <Integer> myList = new ArrayList<Integer>(); 
+        return myList;
     }
 
     /**
@@ -40,7 +42,7 @@ public class ListExercise {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        return list.size();
     }
 
     /**
@@ -52,6 +54,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -67,7 +70,7 @@ public class ListExercise {
      * @return the int at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
-        return 0;
+        return list.get(index);
     }
 
     /**
@@ -80,6 +83,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
+        list.remove(position);
 
     }
 
@@ -93,5 +97,6 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        list.set(position, value);
     }
 }
