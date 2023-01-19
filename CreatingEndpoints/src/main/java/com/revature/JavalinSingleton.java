35c6f42ac93fdd529944189c941b155439ed01bc
S-Yohan <forresters@merrimack.edu>
2023-01-19 00:52:34 +0000

    Saved progress on StatusCodes 2023-01-19 00:52:34 +0000

--- CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
+++ CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
@@ -14,7 +14,8 @@ public class JavalinSingleton {
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
                
-        return app.get("/hello", ctx -> {ctx.result("Hello World");});
+        app.get("/hello", ctx -> {ctx.result("Hello World");});
+        return app;
         
         
 
