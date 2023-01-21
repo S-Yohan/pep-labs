4d8d16c822cc5db8aeef3aa9ea9fc2274a711883
S-Yohan <forresters@merrimack.edu>
2023-01-21 02:49:54 +0000

    Saved progress on IndexOf 2023-01-21 02:49:54 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.List;
 //
 
 public class IndexOf {
@@ -11,6 +12,13 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
-        return -1;
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] == n){
+                return i;
+                
+            }else{
+                return -1;}
+        }
+    
     }
 }
