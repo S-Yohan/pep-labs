acd328a934e3fe9f363b6ff677a6414b34a27984
S-Yohan <forresters@merrimack.edu>
2023-01-25 00:05:37 +0000

    Saved progress on InnerJoin 2023-01-25 00:05:37 +0000

---
+++ InnerJoin/problem1.sql
@@ -0,0 +1,2 @@
+SELECT student.id, student.student_name FROM student INNER JOIN class ON student.class_title = class.class_title 
+WHERE student.class_title = 'Physics' OR student.class_title ='Math';
