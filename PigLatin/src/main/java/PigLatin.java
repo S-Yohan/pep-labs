b79737fc607f41b12febe0e77683be5bb2b3a07b
S-Yohan <forresters@merrimack.edu>
2023-01-31 01:45:23 +0000

    Saved progress on PigLatin 2023-01-31 01:45:23 +0000

--- PigLatin/src/main/java/PigLatin.java
+++ PigLatin/src/main/java/PigLatin.java
@@ -17,6 +17,8 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        String str1 = in.substring(0, 1);
+        String str2 = in.substring(1);
+        return str2 + str1 + "ay";
     }
 }
