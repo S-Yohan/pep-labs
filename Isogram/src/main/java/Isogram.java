afb7af2616ec5b30672e6af811fbc8a7df502971
S-Yohan <forresters@merrimack.edu>
2023-01-31 04:46:38 +0000

    Saved progress on Isogram 2023-01-31 04:46:38 +0000

--- Isogram/src/main/java/Isogram.java
+++ Isogram/src/main/java/Isogram.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Isogram {
     /**
@@ -13,6 +15,14 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        Boolean outcome = false;
+        Set <Character> compareset = new HashSet<Character>();
+        char [] data = str.toCharArray();
+        for(char e: data){
+            compareset.add(e);
+        }
+        if(data.length == compareset.size()){
+            outcome = true;
+        } return outcome;
     }
 }
