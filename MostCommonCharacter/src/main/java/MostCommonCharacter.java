eeb6c38f524aa41ff78ccb815820d26fc10fca89
S-Yohan <forresters@merrimack.edu>
2023-02-02 23:07:53 +0000

    Saved progress on WordCountMap 2023-02-02 23:07:53 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -7,33 +7,36 @@ import java.util.Map.Entry;
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
-     * You could use a HashMap that maps a Character key to an Int value to represent how many times a Character has
+     * You could use a HashMap that maps a Character key to an Int value to
+     * represent how many times a Character has
      * been spotted.
+     * 
      * @param str A String.
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        char CharData [];
+        //Converting the str to a char array
+        char CharData[];
         CharData = str.toCharArray();
+        //Instantiating a HashMap with a key<Character> and a value<Integer>
+        HashMap<Character, Integer> CharCount = new HashMap<Character, Integer>();
 
-        HashMap <Character, Integer> CharCount = new HashMap<Character, Integer>();
-
-        
+        //Output variable is initalized
         char Output = ' ';
-        for(char c: CharData){
+        for (char c : CharData) {
             
-            if(CharCount.containsKey(c)){
+            if (CharCount.containsKey(c)) {
                 CharCount.put(c, CharCount.get(c) + 1);
-            } else{
+            } else {
                 CharCount.put(c, 1);
             }
-        } Integer MostFrequent = Collections.max(CharCount.values());
-        for(Map.Entry<Character, Integer> entry : CharCount.entrySet()){
-            if(entry.getValue() == MostFrequent){
+        }
+        Integer MostFrequent = Collections.max(CharCount.values());
+        for (final Entry entry : CharCount.entrySet()) {
+            if (entry.getValue() == MostFrequent) {
                 Output = (char) entry.getKey();
             }
         }
-        
 
         return Output;
     }
