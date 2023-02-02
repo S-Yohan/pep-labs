36ca9511ca4b147c45c6adfb5003daf95549ffed
S-Yohan <forresters@merrimack.edu>
2023-02-02 22:47:33 +0000

    Saved progress on MostCommonCharacter 2023-02-02 22:47:33 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,30 +6,49 @@ public class InsertPosition {
 
     /**
      * Find the position that a number should be inserted into in a sorted array.
-     * For instance, in the array {2,4,6,8,10}, inserting 5 would belong at position 2, since the resulting array
+     * For instance, in the array {2,4,6,8,10}, inserting 5 would belong at position
+     * 2, since the resulting array
      * would be {2,4,5,6,8,10}, where the index of 5 is 2 (remember zero-indexing.)
      * See the test cases for more examples.
      *
      * You may assume no duplicate values.
      *
-     * @param nums an array of ints.
+     * @param nums   an array of ints.
      * @param target the number that would hypothetically be inserted.
-     * @return return the index that a number should be inserted into a sorted array.
+     * @return return the index that a number should be inserted into a sorted
+     *         array.
      */
     public int searchInsert(int[] nums, int target) {
-        int index = 0;
+
+        // This command sorts nums.
         Arrays.sort(nums);
+
+        // The sorted array will be entered into a larger array to avoid any out of
+        // bound errors.
+        List<Integer> newArray = new ArrayList<>();
+
         for(int i = 0; i< nums.length; i++){
-            if ((Integer.compare(nums[i], target) < 1) && (Integer.compare(nums[i+1], target) > 1)){
-                
-                index = i;
-                
-                
+            newArray.add(nums[i]);
+        }
+        // int newlength = nums.length + nums.length;
+        // int newArray [] = new int [newlength];
+        // System.arraycopy(nums, 0, newArray, 0, nums.length-1);
 
+       // The return object, index, is initialized here to 0;
+        int index = 0;
+
+        // In this for loop the index of that element to be replaced is returned from the
+        // newArray once the conditions are met in the if statement.
+
+        for(int j = 0; j < nums.length; j++){
+            
+            if((newArray.get(j) < target) && (newArray.get(j+1)) > target){
+                newArray.set(j+1, target);
+                index = j+1;
             }
+        }
 
-        }return index;
-        
-        
+        return index;
+
     }
 }
