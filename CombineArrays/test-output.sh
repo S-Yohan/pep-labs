e31b15b1b4376eaaa2cb8c155784c694f5d60bbc
S-Yohan <forresters@merrimack.edu>
2023-02-13 14:29:10 +0000

    Saved progress on CombineArrays 2023-02-13 14:29:10 +0000

--- CombineArrays/test-output.sh
+++ CombineArrays/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running CombineArraysTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.044 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.04 sec
 
 Results :
 
